9620877a8bb58864a1a0b2fe002f10c5148cea84
Ravi Sharma <sraa7877@gmail.com>
2023-03-30 20:58:49 +0530

    day3

--- GropuProject2/product.js
+++ GropuProject2/product.js
@@ -2,28 +2,52 @@ let api = 'https://63c9170d320a0c4c9540575f.mockapi.io/products?_limit=10&page=1
 let form = document.querySelector("#category-filter form")
 let min = document.getElementById("min")
 let clear1 = document.querySelector(".clear")
-let mainbody = document.querySelector("#product-container>div")
+let mainbody = document.querySelector("#product-container")
 let max = document.getElementById("max")
 let priceslider = document.getElementById("priceslider")
-let mensfilter = document.querySelector("#Mens")
-let womensfilter = document.querySelector("#Womens")
 let check = document.querySelectorAll(".check")
+let allproductsfilter = document.querySelector("#allproducts")
+let mensfilter = document.querySelector("#mens")
+let womensfilter = document.querySelector("#womens")
+let kidsfilter = document.querySelector("#kids")
+let bagsfilter = document.querySelector("#bags")
+let decorefilter = document.querySelector("#decore")
+let giftsfilter = document.querySelector("#gifts")
+let nikefilter = document.querySelector("#nike")
+let Adidasfilter = document.querySelector("#Adidas")
+let Louisfilter = document.querySelector("#Louis")
+let Cartierfilter = document.querySelector("#Cartier")
+let Zarafilter = document.querySelector("#Zara")
+let topsfilter = document.querySelector("#topsfilter")
+let jeanfilter = document.querySelector("#jeanfilter")
+let bagpacks = document.querySelector("#bagpacks")
+let searchform = document.querySelector("#form")
+let stars = document.querySelectorAll(".star")
+let output = document.querySelector(".displayrating")
 
 
 
-check.forEach((element)=>{
-  check.addEventListener("change",(event)=>{
-    let ischecked = event.target.checked
-    if(ischecked){
-      console.log(element.value)
+
+
+for(let i=0;i<stars.length;i++){
+  stars[i].starvalue = i+1
+  ["click","mouseover","mouseout"].forEach((e)=>{
+    stars[i].addEventListener(e, showrating)
+  })
+}
+
+function showrating(e){
+  let type = e.type
+  let starvalue = this.starvalue
+
+  stars.forEach((elem,ind)=>{
+    if(type === "click"){
+      if(ind< starvalue){
+        
+      }
     }
   })
-  
-  
-})
-
-
-
+}
 
 
 
@@ -58,39 +82,194 @@ async function fetchandremder(){
 
 
 
-  mensfilter.addEventListener("change",(event)=>{
-    let ischecked = event.target.checked
-    
-    if(ischecked){
-      let filtered1 = data.filter((element)=>{
-        if(element.category=="mens"){
-          return true
+  searchform.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let searchpara = searchform.search.value
+console.log(searchpara)
+    let filtered1 = data.filter((element)=>{
+        if(element.name.toUpperCase().includes(searchpara.toUpperCase())===true){
+            return true
+        }else{
+            return false
         }
-      })
-      mainbody.innerHTML = renderingdata(filtered1)
-    }else{
-      mainbody.innerHTML = renderingdata(data)
-    }
-    
     })
+    mainbody.innerHTML = renderingdata(filtered1)
+  })
 
 
 
-  womensfilter.addEventListener("change",(event)=>{
-    let ischecked = event.target.checked
-    
-    if(ischecked){
-      let filtered1 = data.filter((element)=>{
-        if(element.category=="women"){
-          return true
-        }
-      })
-      mainbody.innerHTML = renderingdata(filtered1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+allproductsfilter.addEventListener("click",()=>{
+  mainbody.innerHTML = renderingdata(data)
+  allproductsfilter.style.backgroundColor = "rgb(17, 17, 153)"
+})
+mensfilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.category=="mens"){
+      return true
     }else{
-      mainbody.innerHTML = renderingdata(data)
+      return false
     }
-    
-    })
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+womensfilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.category=="women"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+kidsfilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.category=="kids"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+bagsfilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.category=="bags"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+decorefilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.category=="homedecore"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+giftsfilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.category=="gifts"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+
+
+
+
+
+
+
+
+nikefilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.brand=="nike"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+Adidasfilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.brand=="adidas"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+Louisfilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.brand=="louis vuitton"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+Cartierfilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.brand=="cartier"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+Zarafilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.brand=="zara"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+
+
+
+
+
+topsfilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.product_badge=="top"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+jeanfilter.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.product_badge=="jeans"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+bagpacks.addEventListener("click",()=>{
+  let filtered = data.filter((ele)=>{
+    if(ele.product_badge=="bags"){
+      return true
+    }else{
+      return false
+    }
+  })
+  mainbody.innerHTML = renderingdata(filtered)
+})
+
 
 
 
@@ -129,14 +308,33 @@ function renderingdata(data){
 }
 function getcard(id,image, title, brand,description, category, price, totalquantity){
   return `
-  <div class="card-item">
-      <img src="${image}" alt="product">
-        <h3>${title}</h3>
-        <p>${brand}</p>
-        <p>${price}</p>
-        <p>Hurry! only <span>${totalquantity}</span> left...</p>
-
-        <button class="catrbtn">Add to cart</button>
+  <div class="card" >
+  <div class="img_div">
+      <a href="">
+        <img src="${image}">
+    </a>
     </div>
-  `
+    <div class="desc">
+      <h2>${title}</h2>
+      <ul class="stars">
+      <li class="star"><i class="fa-solid fa-star"></i></li>
+      <li class="star"><i class="fa-solid fa-star"></i></li>
+      <li class="star"><i class="fa-solid fa-star"></i></li>
+      <li class="star"><i class="fa-solid fa-star"></i></li>
+      <li class="star"><i class="fa-solid fa-star"></i></li>
+      </ul>
+       <p class="displayrating"></p>
+      <h4>
+      ${brand}
+      </h4>
+      <div class="button-div">
+        <a href="">Add to Cart</a>
+        <a href="">Buy Now</a>
+      </div>
+    </div>
+  </div>
+					
+    `
 }
+console.log(stars)
+output.innerHTML = "yes"
